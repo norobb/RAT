d49aee2bd416602b05602be5dc408e8fe4b9a5d8
Norobb <noah.elian.mahyar@gmail.com>
2025-06-19 18:57:01 +0200

    edit

--- compile.bat
+++ compile.bat
@@ -1,4 +1,4 @@
-pyinstaller --noconsole --onefile --paths=C:\Users\krame\.pyenv\pyenv-win\versions\3.13.1\Lib\site-packages -i icon.ico .\client.py
+pyinstaller --noconsole --onefile --paths=C:\Users\%user%\.pyenv\pyenv-win\versions\3.13.1\Lib\site-packages -i icon.ico .\client.py
 
 move dist\client.exe .
 rmdir /s /q dist
